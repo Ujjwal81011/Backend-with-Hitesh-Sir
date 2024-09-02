e98f46cfdb33e79f3e2533bf94b910fa79c5677d
Ujjwal Mishra <mishraujjwal9936@gmail.com>
2024-09-03 01:35:17 +0530

    full stack

---
+++ Backend_connection/frontend/src/App.jsx
@@ -0,0 +1,35 @@
+import { useEffect, useState } from 'react'
+import axios from 'axios';
+import './App.css'
+
+function App() {
+  const [jokes,setJokes] = useState([])
+
+  useEffect(() => {
+    axios.get('http://localhost:3000/api/jokes')
+    .then((response) => {
+      setJokes(response.data)
+    })
+    .catch((error) => {
+      console.log(error);
+      console.log("404 error");
+    })
+  })
+
+  return (
+    <>
+    <h1>Chai aur fullstack</h1>
+    <p>JOKES:{jokes.length}</p>
+    {
+      jokes.map((joke) => {
+        <div key = {joke.id}>
+          <h3>{joke.title}</h3>
+          <p>{joke.content}</p>
+        </div>
+      })
+    }
+    </>
+  )
+}
+
+export default App
