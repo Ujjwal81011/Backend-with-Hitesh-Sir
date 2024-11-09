bc1d8dcf2e3f01df09350404d9c93530b888f5e5
Ujjwal Mishra <mishraujjwal9936@gmail.com>
2024-11-09 23:12:45 +0530

    backend

--- Data_model/config/database.js
+++ Data_model/config/database.js
@@ -6,7 +6,7 @@ const connectDB = async()=>{
     try {
        const response =  await mongoose.connect(process.env.MONGODB_URL)
         // console.log(response);
-        console.log("database chal gaya");
+        console.log("database chal gaya hai");
     } catch (error) {
         console.error(error.message);
         console.log("message krne ka koi fayda hai nhi bekar hai ye");
